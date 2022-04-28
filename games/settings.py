f09acc4964a8d8a3a79a70f3441b33f8901ca66c
Niha Ahmad <nihaahmad26@gmail.com>
2022-04-27 19:14:47 -0500

    static

--- games/settings.py
+++ games/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['nihagamelibrary.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['nihagamelibrary.herokuapp', 'localhost']
 
 
 # Application definition
