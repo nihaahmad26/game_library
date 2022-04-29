f70d25c19a340772e7dc0872a496ef5adc0529eb
Niha Ahmad <nihaahmad26@gmail.com>
2022-04-29 10:14:40 -0500

    local

--- games/settings.py
+++ games/settings.py
@@ -25,9 +25,9 @@ if os.path.isfile(dotenv_file):
 SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['']
+ALLOWED_HOSTS = []
 
 
 # Application definition
